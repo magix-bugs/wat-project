f8a477e0a6a193b16d80827227c668e7a291ddf0
magix-bugs <davienjenga37@gmail.com>
2022-12-01 12:10:07 +0300

    First commit

---
+++ wat/scripts.js
@@ -0,0 +1,16 @@
+let now= new Date(); // Create a new date object
+let hours= now.getHours(); // Find the current hour
+if (hours> 17) {
+    greeting = 'good evening';
+} else if (hours  < 16) {
+    greeting = 'good afternoon';
+}
+else if (hours< 12) {
+    greeting = 'good morning';
+} else {
+    greeting = 'You are Welcome';
+}
+document.write(greeting);
+let spa = getElementById('glamour');
+document.getElementById('glamour');
+document.write(spa);
